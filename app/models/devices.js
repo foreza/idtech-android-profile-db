75930fdbdf746b812f556f28dd96396f90e051f7
David Tran <davidt@itsco.net>
2017-02-14 14:42:05 -0800

    Revert "Move the ProfileSchema into its own file"
    This reverts commit dce1d39a34a4cc325fa1f81144713087534aa34f.

--- app/models/devices.js
+++ app/models/devices.js
@@ -1,7 +1,26 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var ProfileSchema = require('./profiles');
+var ProfileSchema = new Schema({
+    profile_hash: String,
+    input_frq: Number,
+    output_frq: Number,
+    baud: Number,
+    rec_buff_size: Number,
+    volume_adjust: Number,
+    force_hedset: Number,
+    dir_output_wave: Boolean,
+    unimag_ii_suc: Number,
+    unimag_ii_fail: Number,
+    shuttle_suc: Number,
+    shuttle_fail: Number,
+    unipay_suc: Number,
+    unipay_fail: Number,
+    unipay_15_suc: Number,
+    unipay_15_fail: Number,
+    unipay_iii_suc: Number,
+    unipay_iii_fail: Number,
+});
 
 var DeviceSchema   = new Schema({
     manufacture: {
@@ -12,10 +31,7 @@ var DeviceSchema   = new Schema({
         type:String,
         require: true
     },
-    profiles    : [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Profile'
-    }],
+    profiles    : [ProfileSchema],
     meta        : {
         created_at: {
             type: Date,
@@ -30,4 +46,5 @@ var DeviceSchema   = new Schema({
 
 DeviceSchema.index({manufacture: 1, model: 1}, {unique: true});
 
+module.exports = mongoose.model('Profile', ProfileSchema);
 module.exports = mongoose.model('Device', DeviceSchema);
