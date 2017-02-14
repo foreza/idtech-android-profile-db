1823d41ac27e71043f7896fcde83118fed4e967b
David Tran <davidt@itsco.net>
2017-02-14 11:36:57 -0800

    Install crypto-js and use SHA256 for profile_hash

--- app/routes/devices.js
+++ app/routes/devices.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const CryptoJS = require('crypto-js');
+
 // router.post('/')
 //     .post(function(request, response){
 //
@@ -19,12 +21,15 @@ const router = express.Router();
 //     })
 
 router.post('/', (req, res) => {
-	const { input_frq, output_frq, baud,
-			rec_buff_size, volume_adjust, force_hedset,
-			dir_output_wave, unimag_ii_suc, unimag_ii_fail,
-			shuttle_suc, shuttle_fail, unipay_suc,
-			unipay_fail, unipay_15_suc, unipay_15_fail,
-			unipay_iii_suc, unipay_iii_fail } = req.body;
+	const { input_frq = 0, output_frq = 0, baud = 0,
+			rec_buff_size = 0, volume_adjust = 0, force_hedset = 0,
+			dir_output_wave = 0} = req.body;
+
+	const sum = input_frq + output_frq + baud + rec_buff_size + volume_adjust + force_hedset + dir_output_wave;
+
+	const profile_hash = CryptoJS.SHA256(sum.toString()).toString(CryptoJS.enc.Hex);
+
+	res.send(profile_hash);
 });
 
 module.exports = router;
