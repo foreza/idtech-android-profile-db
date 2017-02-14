dce1d39a34a4cc325fa1f81144713087534aa34f
David Tran <davidt@itsco.net>
2017-02-14 12:08:27 -0800

    Move the ProfileSchema into its own file

---
+++ app/models/profiles.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProfileSchema = new Schema({
+    profile_hash: String,
+    input_frq: Number,
+    output_frq: Number,
+    baud: Number,
+    rec_buff_size: Number,
+    volume_adjust: Number,
+    force_hedset: Number,
+    dir_output_wave: Boolean,
+    unimag_ii_suc: Number,
+    unimag_ii_fail: Number,
+    shuttle_suc: Number,
+    shuttle_fail: Number,
+    unipay_suc: Number,
+    unipay_fail: Number,
+    unipay_15_suc: Number,
+    unipay_15_fail: Number,
+    unipay_iii_suc: Number,
+    unipay_iii_fail: Number,
+});
+
+module.exports = mongoose.model('Profile', ProfileSchema);
