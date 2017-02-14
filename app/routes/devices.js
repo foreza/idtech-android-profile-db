2d23144ec8ec191d4db07f9c485e07ba557386b0
David Tran <davidt@itsco.net>
2017-02-14 11:40:47 -0800

    Add check for empty manfacture or model requests

--- app/routes/devices.js
+++ app/routes/devices.js
@@ -3,27 +3,13 @@ const router = express.Router();
 
 const CryptoJS = require('crypto-js');
 
-// router.post('/')
-//     .post(function(request, response){
-//
-//         var device = new Device();
-//         device.model = request.body.model;
-//         device.manufacture = request.body.manufacture;
-//         device.profiles = [];
-//
-//         device.save(function(error){
-//             if (error){
-//                 response.send(error);
-//             }
-//
-//             response.json({message:"device created"});
-//         });
-//     })
-
 router.post('/', (req, res) => {
-	const { input_frq = 0, output_frq = 0, baud = 0,
+	const { manufacture, model, input_frq = 0, output_frq = 0, baud = 0,
 			rec_buff_size = 0, volume_adjust = 0, force_hedset = 0,
 			dir_output_wave = 0} = req.body;
+
+	if (!manufacture || !model)
+		res.sendStatus(400);
 
 	const sum = input_frq + output_frq + baud + rec_buff_size + volume_adjust + force_hedset + dir_output_wave;
 
