9172c2028a46131f35276979d1233118beb959ee
David Tran <davidt@itsco.net>
2017-02-15 15:27:14 -0800

    Refactor POST /api/devices

--- app/routes/api/devices.js
+++ app/routes/api/devices.js
@@ -13,31 +13,23 @@ router.post('/', (req, res) => {
 		.then(device => {
 			// NOTE: It would be great if we can figure out a way to collapse the conditional inside this promise's resolve, the following promises are currently two levels deep (1 level deep would be ideal)
 
-			if (!device) { // Requested device does not exist in collection
-				const newProfile = createNewProfile(req.body);
+			const newProfile = createNewProfile(req.body);
 
+			if (!device) { // Requested device does not exist in collection
 				profilesUtils.createProfile(newProfile)
 					.then(createdProfile => devicesUtils.createDevice({ manufacture, model, profiles: [createdProfile._id] }), () => res.sendStatus(400))
 					.then(() => res.sendStatus(200), () => res.sendStatus(400));
 			} else { // Requested device exists in the collection
-				const { input_frq = 0, output_frq = 0, baud = 0,
-						rec_buff_size = 0, volume_adjust = 0, force_headset = 0,
-						dir_output_wave = false } = req.body;
-
-				const inputProfileHash = profilesUtils.generateSHA256HexString(input_frq + output_frq + baud + rec_buff_size + volume_adjust + force_headset + dir_output_wave);
-
 				/*
 					QUESTION: Is there some way to leverage MongoDB/Mongoose to check if a specific profile exists for the device passed from the previous promise (i.e. 'device' variable)?
 								We are currently just iterating through the profiles array to check
 				*/
-				const profileExists = checkIfProfileExists(device.profiles, inputProfileHash);
+				const profileExists = checkIfProfileExists(device.profiles, newProfile.profile_hash);
 
 				if (profileExists)
 					return res.sendStatus(200); // Sending OK status but do nothing since the profile already exists for the device
 
-				// Here, we know that the device exists but does not contain the input profile so we are adding it
-
-				const newProfile = createNewProfile(req.body);
+				// Here, we know that the device exists but does not contain the new profile so we are adding it
 
 				profilesUtils.createProfile(newProfile)
 					.then(createdProfile => devicesUtils.addProfileForDevice(device._id, createdProfile._id), () => res.sendStatus(400))
