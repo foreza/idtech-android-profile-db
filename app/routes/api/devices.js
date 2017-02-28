784c453b7ec02785a1a2e7e98e0ec2db34631b8b
David Tran <davidt@idtechproducts.com>
2017-02-28 10:05:53 -0800

    Add documentation for GET /api/devices route

--- app/routes/api/devices.js
+++ app/routes/api/devices.js
@@ -4,7 +4,86 @@ const devicesUtils = require('../../utilities/devices');
 const profilesUtils = require('../../utilities/profiles');
 
 // TODO: Secure this route and disallow public usage of this route
-//TODO: commet route
+/**
+ * @api {GET} /api/devices GET /api/devices
+ * @apiDescription Gets a device by manufacture and model if requested; otherwise, gets all devices in the database.
+ * @apiName GetDevices
+ * @apiGroup Devices
+ *
+ * @apiParam {String} [manufacture] Optional device manufacture.
+ * @apiParam {String} [model] Optional device model.
+ *
+ * @apiParamExample {Query} Request-Example (device):
+ * 		/api/devices?manufacture=Samsung&model=S7
+ *
+ * @apiParamExample {Query} Request-Example (devies):
+ * 		/api/devices
+ *
+ * @apiSuccess {Object} device A specific device.
+ *
+ * @apiSuccessExample {JSON} Success-Response-Example (device):
+ *		 {
+ *		   "_id": "58b5b6d71f5e070b14487a79",
+ *		   "manufacture": "Samsung",
+ *		   "model": "S7",
+ *		   "__v": 0,
+ *		   "meta": {
+ *		     "times_requested": 2,
+ *		     "created_at": "2017-02-28T17:43:51.387Z"
+ *		   },
+ *		   "profiles": [
+ *		     {
+ *		       "_id": "58b5b6d71f5e070b14487a78",
+ *		       "profile_hash": "11f372bf5fc808f640fc78fb8565bd6e7efdc0e64d011c07d34317f6dd87fad5",
+ *		       "__v": 0,
+ *		       "meta": {
+ *		         "created_at": "2017-02-28T17:43:51.241Z"
+ *		       },
+ *		       "dir_output_wave": true,
+ *		       "force_headset": 1,
+ *		       "volume_adjust": 1,
+ *		       "rec_buff_size": 64,
+ *		       "baud": 7200,
+ *		       "output_frq": 4800,
+ *		       "input_frq": 2400
+ *		     }
+ *		   ]
+ *		 }
+ *
+ * @apiSuccessExample {JSON} Success-Response-Example (devices):
+ *		 [
+ *		  {
+ *		    "_id": "58b5b6d71f5e070b14487a79",
+ *		    "manufacture": "Samsung",
+ *		    "model": "S7",
+ *		    "__v": 0,
+ *		    "meta": {
+ *		      "times_requested": 2,
+ *		      "created_at": "2017-02-28T17:43:51.387Z"
+ *		    },
+ *		    "profiles": [
+ *		      "58b5b6d71f5e070b14487a78"
+ *		    ]
+ *		  },
+ *		  {
+ *		    "_id": "58b5b9f6e7457120b00a52aa",
+ *		    "manufacture": "Samsung",
+ *		    "model": "S6",
+ *		    "__v": 0,
+ *		    "meta": {
+ *		      "created_at": "2017-02-28T17:57:10.157Z"
+ *		    },
+ *		    "profiles": [
+ *		      "58b5b9f6e7457120b00a52a9"
+ *		    ]
+ *		  }
+ *		]
+ *
+ * @apiSuccess {Object[]} devices All devices in the database
+ *
+ * @apiError (Not Found 404) {String} Error Device was not found.
+ * @apiError (Bad Request 400) {String} Error Request could not be processed.
+ */
 router.get('/', (req, res) => {
 	if(req.query.manufacture && req.query.model){
 		devicesUtils.getDeviceByManufactureAndModelAndPopulateAndIncrementRequestedCount(req.query.manufacture, req.query.model)
